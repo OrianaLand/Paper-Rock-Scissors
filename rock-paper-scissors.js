2f47facd85822e57693f72f0346d72d03c81005e
Oriana <98231498+OrianaLand@users.noreply.github.com>
2025-01-24 14:56:09 -0300

    Fix conditional inside playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,6 +34,19 @@ function getHumanChoice() {
 
 function playRound(playerChoice, machineChoice) {
 
+    if (playerChoice === machineChoice) {
+        return "It's a tie!";
+    }
+
+    switch (playerChoice) {
+        case "rock":
+            return (machineChoice === "paper") ? "Paper beats Rock. You lose!" : "Rock beats Scissors. You win!";
+        case "paper":
+            return (machineChoice === "scissors") ? "Scissors beats Paper. You lose!" : "Paper beats Rock. You win!";
+        case "scissors":
+            return (machineChoice === "rock") ? "Rock beats Scissors. You lose!" : "Scissors beats Paper. You win!";
+    }
+
 }
 
 let humanScore = 0;
